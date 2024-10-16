7c581c02701dfbd6cf22a4c8665be1f826e501de
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2024-10-16 23:35:47 +0200

    add privacy + imprint

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LanguageService } from '../services/language.service';
 
 @Component({
   selector: 'app-imprint',
@@ -9,4 +10,8 @@ import { Component } from '@angular/core';
 })
 export class ImprintComponent {
 
+  source: string = 'assets/icons/back-arrow.png';
+
+  constructor(public lS: LanguageService) {
+  }
 }
