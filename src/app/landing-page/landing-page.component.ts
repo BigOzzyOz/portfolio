0a95b2d56ca18892eded8b1205b639e19873c156
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2024-10-17 19:48:06 +0200

    finish landing page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { LanguageService } from '../services/language.service';
+import { CommonModule } from '@angular/common';
+import { SocialLinksComponent } from "../shared/social-links/social-links.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SocialLinksComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
