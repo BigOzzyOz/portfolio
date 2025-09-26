847736b189a09543eb274bfdd535d3530efefe03
Jan Holtschke <60309436+BigOzzyOz@users.noreply.github.com>
2025-09-26 02:07:43 +0200

    feat: integrate Sentry for error tracking and performance monitoring
    - Added @sentry/angular dependency to package.json.
    - Configured Sentry error handler in app.config.ts.
    - Initialized Sentry in main.ts with DSN and default PII settings.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,28 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, APP_INITIALIZER, ErrorHandler } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import * as Sentry from "@sentry/angular";
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler(),
+    },
+    {
+      provide: Sentry.TraceService,
+      deps: [Router],
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => { },
+      deps: [Sentry.TraceService],
+      multi: true,
+    },
+  ]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import * as Sentry from "@sentry/angular";
+
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+
+Sentry.init({
+  dsn: "https://25eb801249cc0145370949066bd9c92e@o4510077623599104.ingest.de.sentry.io/4510082977366096",
+  sendDefaultPii: true
+});
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
